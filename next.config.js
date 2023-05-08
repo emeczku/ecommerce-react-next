a8b5b18a3cd37bee729e328cfa69773879c16a7f
emeczku <dzius89@gmail.com>
2023-05-08 21:19:56 +0200

    set image unoptimized

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // At the moment, the 4 lines below must exist to be deployed to github pages
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   experimental: {
     appDir: true,
